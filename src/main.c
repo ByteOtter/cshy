246c2405649e22d68b221f6191541f474b504edd
ByteOtter <christopher-hock@protonmail.com>
2024-03-27 16:19:09 +0100

    actually make it usable for testing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include <shy_file.h>
+#include <stdio.h>
 
-int main() {
+int main(int argc, const char** argv) {
+  if (argc <= 2) {
+    fprintf(stderr, "Invalid number of arguments!");
+    return 1;
+  }
+  shy_file file = shy_file_create(argv+1, argc-1);
+  shy_file_save(file, "test.shy");
+
   return 0;
 }
